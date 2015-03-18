305e267c3a2b3460f927fdda61591a6903ba019b
Samyrenr <samyr.enr@gmail.com>
2015-03-18 01:59:48 -0300

    V0.1Teste

---
+++ poo_paint/poo_paint/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo_paint
+{
+    public class retangulo
+    {
+        public int x; public int y; public int l; public int a;
+        static void Main(string[] args)
+        {    
+            retangulo r1 = new retangulo( x, y, l,  a);
+        }
+    }
+}
+
+
